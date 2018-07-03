c4dce76ecec925e348c683e0624e69d66ed954d5
ILIYA <mosienkoiliya120@gmail.com>
2018-07-03 16:04:34 +0300

    Calculator

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace CalculatorX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           double a;
+           double b;
+           double total;
+           char oper;
+           //Мы обьявили переменные
+           Console.WriteLine("Введите первое число");
+           a = Convert.ToDouble(Console.ReadLine());
+           // Пользователь вводит первое число
+            Console.WriteLine("Введите оператор");
+            oper  = Convert.ToChar(Console.ReadLine());
+            //Пользователь вводит оператор
+             Console.WriteLine("Введите второе число");
+            b = Convert.ToDouble(Console.ReadLine());
+            //Пользователь вводит второе  число
+            if (oper == '+')
+            {
+             
+          total = a + b;
+          Console.WriteLine("Cумма " + a + " и " + b + " равна " + total + ".");
+            
+            }
+                    else if (oper == '-')
+                {
+                    total = a - b;
+                    Console.WriteLine("Разность " + a + " и " + b + " равна " + total + ".");
+                }
+ 
+                else if (oper == '*')
+                {
+                    total = a * b;
+                    Console.WriteLine("Умножение " + a + " на " + b + " равно " + total + ".");
+                }
+ 
+                else if (oper == '/')
+                {
+                    total = a / b;
+                    Console.WriteLine("Деление " + a + " на " + b + " равно " + total + ".");
+                }
+                else
+                {
+                   
+                 
+                    Console.WriteLine("Неизвестный оператор.");
+                }
+        
+        }
+
+        private static void NewMethod()
+        {
+            ;
+        }
+    }
+    }
