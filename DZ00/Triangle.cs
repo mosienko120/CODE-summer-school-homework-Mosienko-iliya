29d794ce20c8ca1af5d984592fdf140d85b728c7
mosienko120 <40754196+mosienko120@users.noreply.github.com>
2018-07-03 21:16:38 +0300

    Add files via upload

---
+++ DZ00/Triangle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Travel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x,y; 
+            if (x == 0) {
+                Console.WriteLine("false");
+            }
+            else if (y == 0) {
+                Console.WriteLine("true");
+            }
+            else {
+              Console.WriteLine("");  
+            }
+        }
+    }
+}
