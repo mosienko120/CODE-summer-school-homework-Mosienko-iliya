568bf2d618f9806c24f2ca1b853e3a4bfa6b4e08
mosienko120 <40754196+mosienko120@users.noreply.github.com>
2018-07-05 20:13:34 +0300

    DZzzz

---
+++ DZ01/Сумма и произведение чисел.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Linq;
+ 
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number = new Random().Next(100, 1000);
+        int sum = number.ToString().Select(x => x - '0').
+            Cast<int>().Sum();
+        int mul = number.ToString().Select(x => x - '0').
+            Cast<int>().Aggregate((result, value) => result * value);
+        Console.WriteLine("Число: {0}\nСумма: {1}\nПроизведение: {2}", 
+            number, sum, mul);
+        Console.ReadKey(true);
+    }
+}
+        }
+    }
+}
---
+++ DZ01/Факториал.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+ 
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s;
+            int F = 1;
+            for (s = 5; s > 1; s--)
+                F = F * s;
+            Console.WriteLine("Факториал 5  = " + F);
+ 
+ 
+            Console.ReadLine();
+        }
+    }
+}
